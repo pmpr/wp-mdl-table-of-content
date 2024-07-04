d5ca80d20cdf5afd7dac17e645d899fb770afe5c
pmpr.dev <pmpr.dev@gmail.com>
2024-07-04 20:39:17 +0000

    Obfuscated

--- build/js/editor.vTir-B3Cp.js
+++ build/js/editor.vTir-B3Cp.js
@@ -1,2 +1,2 @@
-/*! For license information please see editor.JMyVn2OH_.js.LICENSE.txt */
+/*! For license information please see editor.vTir-B3Cp.js.LICENSE.txt */
 !function(){const e=PRHelper.getHook(),t=(PRHelper.getType(),PRHelper.getHTML(),PRBackendHelper.getForm());e.on("click","#table_of_content_translate_heading_anchors",(e=>{t.getEditorContent("content")}))}();
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             6684011ee70ee             |
+    |             66870851de5aa             |
     |_______________________________________|
 */
  use Pmpr\Module\TableOfContent\TableOfContent; TableOfContent::symcgieuakksimmu();
